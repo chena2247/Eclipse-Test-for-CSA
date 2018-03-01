6caa519bdd6c7dee7dece7ad2cf5a5d137a1c9b9
chena2247 <annech524@gmail.com>
2018-03-01 08:17:35 -0800

    Some tiny tweaks

--- Unit9/src/DeckTester.java
+++ Unit9/src/DeckTester.java
@@ -18,11 +18,25 @@ public class DeckTester {
 		System.out.println(test.deal());
 		System.out.println(test.deal());
 		System.out.println(test.deal());
-		System.out.println(test.deal());
-		System.out.println(test.deal());
-		System.out.println(test.deal());
-		System.out.println(test.deal());
 
 
+		String[] ranks = {"jack", "queen", "king"};
+		String[] suits = {"blue", "red"};
+		int[] pointValues = {11, 12, 13};
+		
+		Deck second = new Deck(ranks, suits, pointValues);
+		for (int i=0; i<ranks.length*suits.length;i++) {
+			System.out.println(second.deal());
+		}
+		
+		rank = new String[] {"jack", "queen", "king", "ace", "2"};
+		suit = new String[] {"hearts", "spades", "clubs"};
+		pointValue = new int[] {11, 12, 13, 1, 2};
+
+		Deck third = new Deck(ranks, suits, pointValues);
+		for (int i=0; i<ranks.length*suits.length;i++) {
+			System.out.println(third.deal());
+		}
+		
 	}
 }
